fe94ff6c08b4cb4b62b4ed4b78f95a085cd4ac75
Kuldar <hi@kuldar.com>
2022-06-22 16:24:55 +0300

    hm

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,6 +8,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>A-Barber</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
       </Head>
       <Component {...pageProps} />
     </div>
