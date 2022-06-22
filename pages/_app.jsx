0878aecb53f9b09642a73fa03629a00f689de385
Kuldar <hi@kuldar.com>
2022-06-22 16:27:31 +0300

    ok

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         />
-        <meta name="theme-color" content="#ff0000" />>
+        <meta name="theme-color" content="#000" />
       </Head>
       <Component {...pageProps} />
     </div>
